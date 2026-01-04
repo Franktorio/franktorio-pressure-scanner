10c8c58d98e3c95d62c09a343ff5274ee8f3f3d8
Franktorio <lordofroblaxyt@gmail.com>
2026-01-03 19:03:36 -0800

    Implement single-instance enforcement with QSharedMemory

--- main.py
+++ main.py
@@ -7,22 +7,16 @@ from PyQt5.QtCore import QSharedMemory
 from src.app.gui import MainWindow
 from PyQt5.QtWidgets import QApplication
 
-
-
-
 app = QApplication(sys.argv)
 app.setApplicationName("Franktorio Research Scanner")
 app.setApplicationDisplayName("Franktorio Research Scanner")
 
-# Commented out single-instance enforcement for development purposes
-"""
 # Single-instance enforcement
 shared_memory = QSharedMemory("FranktorioScannerInstance")
 if not shared_memory.create(1):
     print("Another instance is already running. Exiting...")
     sys.exit(0)
 shared_memory.attach()
-"""
 
 window = MainWindow()
 window.show()
